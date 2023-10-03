b0ae9f3ac5c55871280a3237b95842dbb224f0f9
Lucas Chen <lucas.chen.formal@gmail.com>
2023-10-03 17:15:24 +0800

    Initial Commit

---
+++ HappyFamily/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  HappyFamily
+//
+//  Created by Lucas Chen on 2023/10/3.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ HappyFamily/HappyFamilyApp.swift
@@ -0,0 +1,17 @@
+//
+//  HappyFamilyApp.swift
+//  HappyFamily
+//
+//  Created by Lucas Chen on 2023/10/3.
+//
+
+import SwiftUI
+
+@main
+struct HappyFamilyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
